cb48351aca9e9fee0a84fd4d148322b1af6e7b2a
Ryan_Mac <ttang@anl.gov>
2018-11-27 20:44:05 -0600

    added and checked all proton potentials

--- potentials.h
+++ potentials.h
@@ -16,6 +16,14 @@ double vsi, rsi0, asi;
 double vso, rso0, aso;
 double vsoi, rsoi0, asoi, rc0;
 
+void PrintPotential(){
+  printf("v    = %7.3f    r0 = %7.3f    a = %7.3f\n", v, r0, a);
+  printf("vi   = %7.3f   ri0 = %7.3f   ai = %7.3f\n", vi, ri0, ai);
+  printf("vsi  = %7.3f  rsi0 = %7.3f  asi = %7.3f\n", vsi, rsi0, asi);
+  printf("vso  = %7.3f  rso0 = %7.3f  aso = %7.3f\n", vso, rso0, aso);
+  printf("vsoi = %7.3f rsoi0 = %7.3f asoi = %7.3f  rc0 = %7.3f\n", vsoi, rsoi0, asoi, rc0);
+}
+
 string potentialRef(string name){
    
   if( name == "A" ){
@@ -67,8 +75,8 @@ bool AnCaiPotential(int A, int Z, double E){
   // 12 < A < 238
   // http://dx.doi.org/10.1103/PhysRevC.73.054605
   
-  if( !(12 <= A &&  A <= 238 ) ) return false;
-  if( E > 183 ) return false;
+  //if( !(12 <= A &&  A <= 238 ) ) return false;
+  //if( E > 183 ) return false;
 
   double A3 = pow(A, 1./3.);
 
@@ -103,15 +111,15 @@ bool HSSPotential(int A, int Z, double E){
   // 12 < A < 209
   // http://dx.doi.org/10.1103/PhysRevC.73.054605
   
-  if( !(12 <= A &&  A <= 209 ) ) return false;
-  if( E > 200 ) return false;
+  //if( !(12 <= A &&  A <= 209 ) ) return false;
+  //if( E > 200 ) return false;
 
-  int N   = A-Z;
+  int N = A-Z;
   double A3 = pow(A, 1./3.);
   
   double VIcond = -4.916 + (0.0555*E) + 0.0000442 * pow(E,2) +  35. * (N-Z)/A;
 
-  v  = 82.18 - 0.148 * E - 0.000886 * pow(E,2) - 34.811*(N-Z)/A + 1.058*N/A;
+  v  = 82.18 - 0.148 * E - 0.000886 * pow(E,2) - 34.811*(N-Z)/A + 1.058*Z/A3;
   r0 = 1.174;
   a  = 0.809;
 
@@ -132,6 +140,8 @@ bool HSSPotential(int A, int Z, double E){
   asoi  = 0.813;
 
   rc0 = 1.698;
+  
+  //PrintPotential();
 
   return true;
 }
@@ -144,8 +154,8 @@ bool KoningPotential(int A, int Z, double E){
   // 24 < A < 209
   // http://dx.doi.org/10.1016/S0375-9474(02)01321-0
   
-  if( !(24 <= A &&  A <= 209 ) ) return false;
-  if( E > 200 ) return false;
+  //if( !(24 <= A &&  A <= 209 ) ) return false;
+  //if( E > 200 ) return false;
 
   int N   = A-Z;
   double A3 = pow(A, 1./3.);
@@ -210,15 +220,187 @@ bool KoningPotential(int A, int Z, double E){
   return true;
 }
 
+bool VarnerPotential(int A, int Z, double E){
+  // p + A(Z)
+  // 16 < E < 65 or 65 MeV/u
+  // 4 < A < 209
+  // http://dx.doi.org/10.1016/0370-1573(91)90039-O
+  
+  //if( !(4 <= A &&  A <= 209 ) ) return false;
+  //if( E > 65 || E < 16 ) return false;
+
+  int N   = A-Z;
+  double A3 = pow(A, 1./3.);
+  
+  double Rc = 1.24 * A3 + 0.12;
+  double EC = 1.73 * Z / Rc;
+  double ETA = 1.0*(N-Z)/A;
+  
+  printf("Rc  : %f \n", Rc);
+  printf("EC  : %f \n", EC);
+  printf("ETA : %f \n", ETA);
+  
+  v  = 52.9 + 13.1 * (N-Z)/A - 0.299 * (E - EC);
+  r0 = 1.25 - 0.225/A3;
+  a  = 0.690;
+
+  vi  = 7.8/(1 + exp((35. - E + EC)/16.) );
+  ri0 = 1.33 - 0.42/A3;
+  ai  = 0.69;
+
+  vsi  = (10 + 18.*(N-Z)/A)/(1+exp((E-EC - 36.)/37.));
+  rsi0 = 1.33 - 0.42/A3;
+  asi  = 0.69;
+
+  vso  = 5.9;
+  rso0 = 1.34 - 1.2/A3;
+  aso  = 0.63;
+
+  vsoi  = 0;
+  rsoi0 = 0;
+  asoi  = 0;
+
+  rc0 = Rc/A3;
+  
+  PrintPotential();
+
+  return true;
+}
+
+bool MenetPotential(int A, int Z, double E){
+  // p + A(Z)
+  // 30 < E < 60 or 60 MeV/u
+  // 40 < A 
+  // http://dx.doi.org/10.1016/0370-1573(91)90039-O
+  
+  //if( !(40 <= A ) ) return false;
+  //if( E > 60 || E < 30 ) return false;
+
+  int N   = A-Z;
+  double A3 = pow(A, 1./3.);
+  
+  v  = 49.9 - 0.22 * E + 26.4 * (N-Z) / A + 0.4 * Z / A3;
+  r0 = 1.16;
+  a  = 0.75;
+
+  vi  = 1.2 + 0.09 * E;
+  ri0 = 1.37;
+  ai  = 0.74 - 0.008 * E + 1.*(N-Z)/A;
+
+  vsi  = 4.2 - 0.05 * E + 15.5 * (N-Z)/A;
+  rsi0 = 1.37;
+  asi  = 0.74 - 0.008 * E + 1.*(N-Z)/A;
+
+  vso  = 6.04;
+  rso0 = 1.064;
+  aso  = 0.78;
+
+  vsoi  = 0;
+  rsoi0 = 0;
+  asoi  = 0;
+
+  rc0 = 1.25;
+  
+  //PrintPotential();
+
+  return true;
+}
+
+bool BecchettiPotential(int A, int Z, double E){
+  // p + A(Z)
+  // E < 50 or 60 MeV/u
+  // 40 < A 
+  // http://dx.doi.org/10.1103/PhysRev.182.1190
+  
+  //if( !(40 <= A ) ) return false;
+  //if( E > 60 || E < 30 ) return false;
+
+  int N   = A-Z;
+  double A3 = pow(A, 1./3.);
+  
+  v  = 54 - 0.32 * E + 24. * (N-Z) / A + 0.4 * Z / A3;
+  r0 = 1.17;
+  a  = 0.75;
+
+  vi  = 0.22 * E - 2.7 < 0 ? 0 : 0.22 * E - 2.7;
+  ri0 = 1.32;
+  ai  = 0.51 + 0.7 * (N-Z)/A;
+
+  vsi  = 4.2 - 0.05 * E + 15.5 * (N-Z)/A;
+  rsi0 = 11.8 - 0.258 * E + 12. * (N-Z)/A;
+  if( rsi0 < 0 ) {
+    rsi0 = 0;
+  }
+  asi  = 0.51 + 0.7 * (N-Z)/A;
+
+  vso  = 6.2;
+  rso0 = 1.1;
+  aso  = 0.75;
+
+  vsoi  = 0;
+  rsoi0 = 0;
+  asoi  = 0;
+
+  rc0 = 1.3;
+  
+  //PrintPotential();
+
+  return true;
+}
+
+bool PereyPotential(int A, int Z, double E){
+  // p + A(Z)
+  // E < 20 or 20 MeV/u
+  // 30 < A < 100 
+  // http://dx.doi.org/10.1103/PhysRev.182.1190
+  
+  //if( !(40 <= A && A <= 100 ) ) return false;
+  //if( E > 20 ) return false;
+
+  int N   = A-Z;
+  double A3 = pow(A, 1./3.);
+  
+  v  = 53.3 - 0.55 * E + 27. * (N-Z) / A + 0.4 * Z / A3;
+  r0 = 1.25;
+  a  = 0.65;
+
+  vi  = 0.;
+  ri0 = 0.;
+  ai  = 0.;
+
+  vsi  = 13.5;
+  rsi0 =  1.25;
+  asi  = 0.47;
+
+  vso  = 7.5;
+  rso0 = 1.25;
+  aso  = 0.47;
+
+  vsoi  = 0;
+  rsoi0 = 0;
+  asoi  = 0;
+
+  rc0 = 1.25;
+  
+  PrintPotential();
+
+  return true;
+}
 
 
 bool CallPotential(string potName, int A, int Z, double E){
   bool okFlag = false;
-
-  if( potName == "A") AnCaiPotential(A, Z, E);
-  if( potName == "H") HSSPotential(A, Z, E);
   
-  if( potName == "K") KoningPotential(A, Z, E);
+  //printf(" Potenital : %s \n", potName.c_str());
+
+  if( potName == "A") okFlag = AnCaiPotential(A, Z, E);
+  if( potName == "H") okFlag = HSSPotential(A, Z, E);
+  
+  if( potName == "K") okFlag = KoningPotential(A, Z, E);
+  if( potName == "V") okFlag = VarnerPotential(A, Z, E);
+  if( potName == "M") okFlag = MenetPotential(A, Z, E);
+  if( potName == "G") okFlag = BecchettiPotential(A, Z, E);
+  if( potName == "P") okFlag = PereyPotential(A, Z, E);
   
   return okFlag;
 }
